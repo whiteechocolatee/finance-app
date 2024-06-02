fa98e8e4a6d4e46961f6bdc159777b301a74aff4
Andrey Chorniy <creatuseandr@icloud.com>
2024-06-02 14:52:44 +0300

    create ability to create user bank accounts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <QueryProvider>
             <SheetProvider />
+            <Toaster />
             {children}
           </QueryProvider>
         </body>
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -5,10 +5,28 @@ import {
   SheetHeader,
   SheetTitle,
 } from '@/components/ui/sheet';
+import { insertAccountSchema } from '@/db/schema';
+import { useCreateAccount } from '@/features/accounts/api/use-create-account';
 import { useNewAccount } from '@/features/accounts/hooks/use-new-account';
+import * as z from 'zod';
+import { AccountForm } from './account-form';
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
@@ -19,6 +37,13 @@ export const NewAccountSheet = () => {
             Create a new account to track your transactions
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          defaultValues={{
+            name: '',
+          }}
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+        />
       </SheetContent>
     </Sheet>
   );
