152bb6a2e319b0bbf637fcbd21555c745e8bc302
Andrey Chorniy <creatuseandr@icloud.com>
2024-06-12 16:13:22 +0300

    create transaction hooks and create new api for bulk create transactions

---
+++ features/transactions/components/account-form.tsx
@@ -0,0 +1,84 @@
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { insertAccountSchema } from '@/db/schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Trash } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const AccountForm = ({
+  id,
+  defaultValues,
+  disabled,
+  onSubmit,
+  onDelete,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    onSubmit(values);
+  };
+  const handleDelete = () => {
+    onDelete?.();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className='space-y-4 pt-4'
+        onSubmit={form.handleSubmit(handleSubmit)}>
+        <FormField
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder='ex. Cash, Bank'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className='w-full' disabled={disabled}>
+          {id ? 'Save changes' : 'Create an account'}
+        </Button>
+        {!!id && (
+          <Button
+            type='button'
+            disabled={disabled}
+            onClick={handleDelete}
+            className='w-full'
+            variant='outline'>
+            <Trash className='size-4 mr-2' />
+            Delete account
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
