c679e972283eb7ca833f949951d535e504c20c90
Andrey Chorniy <creatuseandr@icloud.com>
2024-06-13 18:57:09 +0300

    create transactions csv import

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -15,8 +15,43 @@ import { useBulkDeleteTransactions } from '@/features/transactions/api/use-bulk-
 import { useGetTransactions } from '@/features/transactions/api/use-get-transactions';
 import { useNewTransaction } from '@/features/transactions/hooks/use-new-transaction';
 import { columns } from './columns';
+import { useState } from 'react';
+import { UploadButton } from './upload-button';
+import { ImportCard } from './import-card';
+
+enum VARIANTS {
+  LIST = 'LIST',
+  IMPORT = 'IMPORT',
+}
+
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 const TransactionsPage = () => {
+  const [variant, setVariant] = useState<VARIANTS>(
+    VARIANTS.LIST,
+  );
+  const [importResults, setImportResults] = useState(
+    INITIAL_IMPORT_RESULTS,
+  );
+
+  const onUpload = (
+    results: typeof INITIAL_IMPORT_RESULTS,
+  ) => {
+    console.log({ results });
+
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+
   const newTransaction = useNewTransaction();
   const deleteTransactions = useBulkDeleteTransactions();
   const transactionQuery = useGetTransactions();
@@ -43,6 +78,18 @@ const TransactionsPage = () => {
     );
   }
 
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
       <Card className='border-none drop-shadow-sm'>
@@ -50,10 +97,16 @@ const TransactionsPage = () => {
           <CardTitle className='text-xl line-clamp-1'>
             Transactions History
           </CardTitle>
-          <Button onClick={newTransaction.onOpen} size='sm'>
-            <Plus className='size-4 mr-2' />
-            Add new
-          </Button>
+          <div className='flex flex-col lg:flex-row gap-y-2 items-center gap-x-2'>
+            <Button
+              className='w-full lg:w-auto'
+              onClick={newTransaction.onOpen}
+              size='sm'>
+              <Plus className='size-4 mr-2' />
+              Add new
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
---
+++ app/(dashboard)/transactions/upload-button.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@/components/ui/button';
+import { Upload, UploadIcon } from 'lucide-react';
+import { useCSVReader } from 'react-papaparse';
+
+type Props = {
+  onUpload: (results: any) => void;
+};
+
+export const UploadButton = ({ onUpload }: Props) => {
+  const { CSVReader } = useCSVReader();
+
+  return (
+    <CSVReader onUploadAccepted={onUpload}>
+      {({ getRootProps }: any) => (
+        <Button
+          size='sm'
+          className='w-full lg:w-auto'
+          {...getRootProps()}>
+          <UploadIcon className='size-4 mr-2' />
+          Import
+        </Button>
+      )}
+    </CSVReader>
+  );
+};
